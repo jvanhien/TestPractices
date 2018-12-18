87f020d13e9fa14505be9d0e2db817739f496eb1
La Anh Tu <laanhtu@live.co.uk>
2018-12-19 00:45:43 +0700

    Test Customer Controller class

---
+++ TestPractices/NUnit.TestPractices.Tests/CustomerControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestPractices.Fundamentals;
+
+namespace NUnit.TestPractices.Tests
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            var customerController = new CustomerController();
+
+            var actionResult = customerController.GetCustomer(0);
+
+            Assert.That(actionResult, Is.TypeOf(typeof(NotFound)));
+        }
+
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnOK()
+        {
+            var customerController = new CustomerController();
+
+            var actionResult = customerController.GetCustomer(1);
+
+            Assert.That(actionResult, Is.TypeOf<Ok>());
+        }
+    }
+}
